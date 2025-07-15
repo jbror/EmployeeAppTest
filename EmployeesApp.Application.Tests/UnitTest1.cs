27234b31e179ced7f044a29c8a8dd0ac94e4aedc
bror <bror@DESKTOP-PS85NM2>
2025-07-15 17:01:51 +0200

    Working on my test. Adding Arrange, Act and Assert with my logic. Still work in progress

--- EmployeesApp.Application.Tests/UnitTest1.cs
+++ EmployeesApp.Application.Tests/UnitTest1.cs
@@ -1,37 +1,59 @@
-﻿namespace EmployeesApp.Application.Tests
+﻿using EmployeesApp.Application.Employees.Services;
+using EmployeesApp.Application.Employees.Interfaces;
+using EmployeesApp.Domain.Entities;
+
+namespace EmployeesApp.Application.Tests;
+
+public class EmployeeServiceTests
 {
-    public class EmployeeServiceTests
+    [Fact]
+    public void GetById_ValidId_ReturnsEmployee()
     {
-        [Fact]
-        public void GetBy_ValidId_ReturnsEmployee()
+        // Arrange
+        
+        var employeeService = new EmployeeService(new testEmployeeRepository());
+
+
+        // Act
+
+        var result = employeeService.GetById(1);
+
+
+
+        // Assert
+
+        Assert.NotNull(result);
+        Assert.Equal(1, result!.Id);
+        Assert.Equal("Hasse", result.Name);
+
+
+    }
+
+    class TestEmployeeRepository : IEmployeeRepository
+    {
+        private readonly List<Employee> _employees = new()
         {
-            // Arrange
-            
-            var employeeService = new EmployeeService(new testEmployeeRepository());
+
+         new Employee {Id = 1, Name = "Hasse"},
+         new Employee {Id = 2, Name = "Brasse"}
 
 
-            // Act
+        };
 
 
+        public void Add(Employee employee)
+        {
+            _employees.Add(employee);
+        }
 
+        public Employee[] GetAll()
+        {
+            throw new NotImplementedException();
+        }
 
-            // Assert
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        public Employee? GetById(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
